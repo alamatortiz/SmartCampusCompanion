73d96fb6ef17e06c218d1ac962666aeff1b43d9b
Gab Pantaleon <eloisahpapagayo@gmail.com>
2026-02-01 01:45:13 +0800

    Updated campus data by adding their own pictures + added new pictures to drawables - papagayo

--- app/src/main/java/com/example/smartcampuscompanion/data/CampusData.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/CampusData.kt
@@ -1,12 +1,29 @@
 package com.example.smartcampuscompanion.data
 
+import com.example.smartcampuscompanion.R
 import com.example.smartcampuscompanion.model.Department
 
 object CampusData {
     val departments = listOf(
-        Department("Computer Science", "cs@university.edu"),
-        Department("Engineering", "eng@university.edu"),
-        Department("Business", "business@university.edu"),
-        Department("Registrar", "registrar@university.edu")
+        Department(
+            name = "Computer Science",
+            contact = "cs@university.edu",
+            imageRes = R.drawable.ccs
+        ),
+        Department(
+            name = "Engineering",
+            contact = "eng@university.edu",
+            imageRes = R.drawable.eng
+        ),
+        Department(
+            name = "Business",
+            contact = "business@university.edu",
+            imageRes = R.drawable.business
+        ),
+        Department(
+            name = "Registrar",
+            contact = "registrar@university.edu",
+            imageRes = R.drawable.registrar
+        )
     )
 }
