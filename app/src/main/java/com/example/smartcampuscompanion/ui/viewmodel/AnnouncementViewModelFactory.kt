4c050b8e760abebd2d1d2d307f1c5ff46adba837
Gab Pantaleon <pantaleongabriela95@gmail.com>
2026-04-26 01:24:13 +0800

    New AnnouncementViewModelFactory with Connection to firebase - Pantaleon

--- app/src/main/java/com/example/smartcampuscompanion/ui/viewmodel/AnnouncementViewModelFactory.kt
+++ app/src/main/java/com/example/smartcampuscompanion/ui/viewmodel/AnnouncementViewModelFactory.kt
@@ -1,14 +1,20 @@
 package com.example.smartcampuscompanion.ui.viewmodel
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.smartcampuscompanion.data.local.AnnouncementDao
+import com.example.smartcampuscompanion.data.repository.AnnouncementRepository
 
-class AnnouncementViewModelFactory(private val announcementDao: AnnouncementDao) : ViewModelProvider.Factory {
+class AnnouncementViewModelFactory(
+    private val repository: AnnouncementRepository,
+    private val prefs: SharedPreferences,
+    private val context: Context
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AnnouncementViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return AnnouncementViewModel(announcementDao) as T
+            return AnnouncementViewModel(repository, prefs, context) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
