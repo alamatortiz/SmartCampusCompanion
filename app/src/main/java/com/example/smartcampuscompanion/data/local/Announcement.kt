24865a33a545c4262d1148dd2227fd8477f9ff20
Gab Pantaleon <pantaleongabriela95@gmail.com>
2026-04-26 01:27:11 +0800

    New Announcement with Connection to firebase - Pantaleon

--- app/src/main/java/com/example/smartcampuscompanion/data/local/Announcement.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/local/Announcement.kt
@@ -1,11 +1,8 @@
 package com.example.smartcampuscompanion.data.local
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "announcements")
 data class Announcement(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val id: Int = 0,
+    val documentId: String = "",
     val title: String,
     val content: String,
     val date: Long,
