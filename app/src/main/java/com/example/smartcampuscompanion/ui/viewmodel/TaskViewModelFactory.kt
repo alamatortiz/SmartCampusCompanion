777ef5b04ca53cd58e4ee6328feab577a2c66e6c
Gab Pantaleon <paralaandreivincent46@gmail.com>
2026-04-26 01:21:26 +0800

    New TaskViewModelFactory with Connection to firebase - Parala

--- app/src/main/java/com/example/smartcampuscompanion/ui/viewmodel/TaskViewModelFactory.kt
+++ app/src/main/java/com/example/smartcampuscompanion/ui/viewmodel/TaskViewModelFactory.kt
@@ -2,13 +2,17 @@ package com.example.smartcampuscompanion.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.smartcampuscompanion.data.local.*
+import com.example.smartcampuscompanion.data.repository.AuthRepository
+import com.example.smartcampuscompanion.data.repository.TaskRepository
 
-class TaskViewModelFactory(private val taskDao: TaskDao) : ViewModelProvider.Factory {
+class TaskViewModelFactory(
+    private val taskRepository: TaskRepository,
+    private val authRepository: AuthRepository
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TaskViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return TaskViewModel(taskDao) as T
+            return TaskViewModel(taskRepository, authRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
